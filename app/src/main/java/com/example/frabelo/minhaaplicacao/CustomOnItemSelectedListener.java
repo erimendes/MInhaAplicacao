f4e0c1a28438b1eea3270d5e1fd8defcef81ed15
erimendes <eri.mendes32@gmail.com>
2015-06-17 10:43:14 -0300

    Minha primeira aplicação

---
+++ app/src/main/java/com/example/frabelo/minhaaplicacao/CustomOnItemSelectedListener.java
@@ -0,0 +1,21 @@
+package com.example.frabelo.minhaaplicacao;
+
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+/**
+ * Created by FRabelo on 17/06/2015.
+ */
+public class CustomOnItemSelectedListener implements android.widget.AdapterView.OnItemSelectedListener {
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        Toast.makeText(parent.getContext(), "OnItemSelectedListener : "
+                + parent.getItemAtPosition(pos).toString(),
+                Toast.LENGTH_SHORT).show();
+        }
+
+    public void onNothingSelected(AdapterView<?> arg0) {
+
+    }
+}
